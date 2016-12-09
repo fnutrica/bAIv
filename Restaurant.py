e878f9a60a4a906104d8b8cd6a15f90654595001
FRANCESCO NUTRICATO <fnutrica@macalester.edu>
2016-12-09 00:47:38 -0600

    refactoring and changed cost structure

--- Restaurant.py
+++ Restaurant.py
@@ -7,7 +7,7 @@ LOCATION = {"Urban": 30, "Sub": 25, "Rural": 20}
 
 class Restaurant(object):
 
-    def __init__(self, score = 0, food_type = None, location = None, service_lv = None):
+    def __init__(self, score=0, food_type=None, location=None, service_lv=None):
 
         # Resturant Score
         if score != 0:
@@ -16,31 +16,31 @@ class Restaurant(object):
             self.score = 2.5
 
         # Service Level
-        if service_lv != None:
+        if service_lv is not None:
             self.service_lv = service_lv
         else:
             self.service_lv = random.randint(0, 100)
 
         # Food Type
-        if food_type != None:
+        if food_type is not None:
             self.food_type = food_type
         else:
             self.food_type = random.choice(FOOD_TYPE.keys())
 
         # Food Type
-        if location != None:
+        if location is not None:
             self.location = location
         else:
             self.location = random.choice(LOCATION.keys())
 
-        self.price = unit_price()
-        self.cost = unit_cost()
+        self.price = self.unit_price()
+        self.cost = self.unit_cost()
 
     def unit_price(self):
         self.price = 100
         return self.price
 
-    def unit_cost(self):
+    def tot_cost(self, q_sold):
         #Cost of Food
         food = FOOD_TYPE.get(self.food_type)
         
@@ -50,7 +50,7 @@ class Restaurant(object):
         # Cost of Labor
         labor = int(self.service_lv * 0.3)
 
-        self.cost = food + rent + labor
+        self.cost = food*q_sold + rent + labor*q_sold
 
         return self.cost
 
