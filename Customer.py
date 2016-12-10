ab0b981a51467eaad6c25bade411afb222c2fc9b
FRANCESCO NUTRICATO <fnutrica@macalester.edu>
2016-12-10 01:41:46 -0600

    adapted customer class new model specification

--- Customer.py
+++ Customer.py
@@ -3,22 +3,42 @@ import random
 
 class Customer(object):
 
-    def __init__(self, food_pref, service_val, location_pref, pe_happiness):
-        self.food_pref = {"Pizza": random.randint(0, 1), "Indian": random.randint(0, 1), "Burger": random.randint(0, 1),
-                          "Shish": random.randint(0, 1)}
-        self.food_pref = food_pref
-        self.service_val = service_val
-        self.location_pref = location_pref
-        self.pe_happiness = pe_happiness
-        self.expected_scores = None
+    def __init__(self, food_pref=None, service_val=None, location_pref=None, pe_happiness=None):
 
+        if food_pref is None:
+            self.food_pref = {"Pizza": random.uniform(0, 1), "Indian": random.uniform(0, 1),
+                              "Burger": random.uniform(0, 1), "Shish": random.uniform(0, 1)}
+        else:
+            self.food_pref = food_pref
+
+        if self.service_val is None:
+            self.service_val = random.uniform(0, 1)
+        else:
+            self.service_val = service_val
+
+        if food_pref is None:
+            self.location_pref = {"Urban": random.uniform(0, 1), "Sub": random.uniform(0, 1),
+                                  "Rural": random.uniform(0, 1)}
+        else:
+            self.location_pref = location_pref
+
+        if pe_happiness is None:
+            self.pe_happiness = random.uniform(0, 1)
+        else:
+            self.pe_happiness = pe_happiness
+
+        self.expected_scores = []
+
+    # service level currently carries a lot less weight than the other 2 params
     def set_expectations(self, restaurant):
-        self.expected_scores[id(restaurant)] = self.food_pref[restaurant.food_type] + \
+        self.expected_scores.push({"restaurant": restaurant, "expectation":
+                                                self.food_pref[restaurant.food_type] + \
                                                self.service_val * restaurant.service_lv + \
-                                               self.location_pref[restaurant.location]
+                                               self.location_pref[restaurant.location]})
         return
 
     def score_restaurant(self, restaurant):
+
         if restaurant.tip:
             additional_price = random.randint(-10, 10)
             actual_score = self.expected_scores[restaurant] + additional_price*self.pe_happiness
