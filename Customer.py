09ef2749dbd92b54951f784d37425ee4b737d52b
FRANCESCO NUTRICATO <fnutrica@macalester.edu>
2016-12-11 01:09:13 -0600

    fixed customer class indentation

--- Customer.py
+++ Customer.py
@@ -4,9 +4,9 @@ import random
 class Customer(object):
 
     def __init__(self, food_pref={"Pizza": random.uniform(0, 1), "Indian": random.uniform(0, 1), "Burger": random.uniform(0, 1), "Shish": random.uniform(0, 1)},
-                        service_val=random.uniform(0, 1),
-                        location_pref={"Urban": random.uniform(0, 1), "Sub": random.uniform(0, 1),"Rural": random.uniform(0, 1)},
-                        pe_happiness=random.uniform(0, 1)):
+                 service_val=random.uniform(0, 1),
+                 location_pref={"Urban": random.uniform(0, 1), "Sub": random.uniform(0, 1),"Rural": random.uniform(0, 1)},
+                 pe_happiness=random.uniform(0, 1)):
 
         self.food_pref = food_pref
         self.service_val = service_val
@@ -30,10 +30,7 @@ class Customer(object):
 
     # service level currently carries a lot less weight than the other 2 params
     def set_expectations(self, restaurant):
-        self.expected_scores.append({"restaurant": restaurant, "expectation":
-            self.food_pref[restaurant.food_type] + \
-            self.service_val * restaurant.service_lv + \
-            self.location_pref[restaurant.location]})
+        self.expected_scores.append({"restaurant": restaurant, "expectation":self.food_pref[restaurant.food_type] + self.service_val * restaurant.service_lv + self.location_pref[restaurant.location]})
         return
 
     def score_restaurant(self, choice):
