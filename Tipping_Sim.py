05ed4fc9fdd8c6d5c738ed48928a4b640cc26131
Tianyi Liu <tliu@macalester.edu>
2017-02-17 13:52:12 -0600

    added main fucntion

--- Tipping_Sim.py
+++ Tipping_Sim.py
@@ -12,7 +12,102 @@ RESTAURANT_TYPES = ["Pizza  ", "Burger ", "African", "Shish  ", "Asian  "]
 TYPE_COUNTS = {"Pizza  ": 0, "Burger ": 0, "African": 0, "Shish  ": 0,"Asian  ": 0}
 TIPPING_COUNTS = {"Pizza  ": 0, "Burger ": 0, "African": 0, "Shish  ": 0,"Asian  ": 0}
 
-def tipping_sim():
+def printRestaurants(neighList):
+    for neigh in neighList:
+        neigh.printRestaurant()
+
+def countRestaurants(restaurant_pop):
+    global RESTAURANT_TYPES, TYPE_COUNTS
+    for r in restaurant_pop:
+        r.update_tipping()
+        for i in range(5):
+            if r.food_type == RESTAURANT_TYPES[i]:
+                TYPE_COUNTS[RESTAURANT_TYPES[i]] += 1
+                countTipping(r)
+
+def resetCounts():
+    global TYPE_COUNTS, TIPPING_COUNTS
+    newDict1 = TYPE_COUNTS.fromkeys(TYPE_COUNTS, 0)
+    newDict2 = TIPPING_COUNTS.fromkeys(TIPPING_COUNTS, 0)
+    TYPE_COUNTS = newDict1
+    TIPPING_COUNTS = newDict2
+
+def countTipping(restaurant):
+    global TIPPING_COUNTS
+    if restaurant.tipping is True:
+        TIPPING_COUNTS[restaurant.food_type] += 1
+
+# ============================================ GA Helper Fucntions ================================================
+
+def rouletteSelect(valueList):
+    totalValues = sum(valueList)
+    pick = random.uniform(0, totalValues)
+    s = 0
+    for i in range(len(valueList)):
+        s += valueList[i]
+        if s >= pick:
+            return i
+    return len(valueList) - 1
+
+
+def selectParents(states, fitnesses):
+    """given a set of states, repeatedly select parents using roulette selection"""
+    parents = []
+    for i in range(len(states)):
+        nextParentPos = rouletteSelect(fitnesses)
+        parents.append(states[nextParentPos])
+    return parents
+
+
+def mateParents(parents):
+    newPop = []
+
+    # select parents for crossover
+    for i in range(0, len(parents), 2):
+        p1 = parents[i]
+        p2 = parents[i + 1]
+
+        # exactly 4 attributes to be swapped for both Customer and Restaurant
+        n_cross = random.randint(0, N_ATTRIBUTES)
+
+        if n_cross == 0:
+            newPop.append(p1)
+            newPop.append(p2)
+        else:
+            newOnes = crossover(p1, p2, n_cross)
+            newPop.extend(newOnes)
+
+    # mutate child
+    for i in range(len(newPop)):
+        nextOne = newPop[i]
+        doMutate = random.random()
+
+        # 50% chance of mutation
+
+        if doMutate <= 0.1:
+            nextOne.mutate()
+            newPop[i] = nextOne
+
+    return newPop
+
+def crossover(agent1, agent2, n_cross):
+    prob_cross = random.randint(0, 100)
+
+    if prob_cross <= 101:  # for future use
+
+        targets = random.sample(range(0, N_ATTRIBUTES), n_cross)
+
+        for i in targets:
+            attribute1 = agent1.get_e_attribute(i)
+            attribute2 = agent2.get_e_attribute(i)
+            agent1.set_e_attribute(i, attribute2)
+            agent2.set_e_attribute(i, attribute1)
+
+    return [agent1, agent2]
+
+# ============================================= Main Function ===============================================
+
+def main():
     sim_results = {}
     customer_pop = []
     restaurant_pop = []
@@ -98,98 +193,4 @@ def tipping_sim():
             TIPPING_COUNTS[RESTAURANT_TYPES[i]])
 
     #return sim_results
-
-def printRestaurants(neighList):
-    for neigh in neighList:
-        neigh.printRestaurant()
-
-def countRestaurants(restaurant_pop):
-    global RESTAURANT_TYPES, TYPE_COUNTS
-    for r in restaurant_pop:
-        r.update_tipping()
-        for i in range(5):
-            if r.food_type == RESTAURANT_TYPES[i]:
-                TYPE_COUNTS[RESTAURANT_TYPES[i]] += 1
-                countTipping(r)
-
-def resetCounts():
-    global TYPE_COUNTS, TIPPING_COUNTS
-    newDict1 = TYPE_COUNTS.fromkeys(TYPE_COUNTS, 0)
-    newDict2 = TIPPING_COUNTS.fromkeys(TIPPING_COUNTS, 0)
-    TYPE_COUNTS = newDict1
-    TIPPING_COUNTS = newDict2
-
-def countTipping(restaurant):
-    global TIPPING_COUNTS
-    if restaurant.tipping is True:
-        TIPPING_COUNTS[restaurant.food_type] += 1
-
-# ============================================================================================
-
-def rouletteSelect(valueList):
-    totalValues = sum(valueList)
-    pick = random.uniform(0, totalValues)
-    s = 0
-    for i in range(len(valueList)):
-        s += valueList[i]
-        if s >= pick:
-            return i
-    return len(valueList) - 1
-
-
-def selectParents(states, fitnesses):
-    """given a set of states, repeatedly select parents using roulette selection"""
-    parents = []
-    for i in range(len(states)):
-        nextParentPos = rouletteSelect(fitnesses)
-        parents.append(states[nextParentPos])
-    return parents
-
-
-def mateParents(parents):
-    newPop = []
-
-    # select parents for crossover
-    for i in range(0, len(parents), 2):
-        p1 = parents[i]
-        p2 = parents[i + 1]
-
-        # exactly 4 attributes to be swapped for both Customer and Restaurant
-        n_cross = random.randint(0, N_ATTRIBUTES)
-
-        if n_cross == 0:
-            newPop.append(p1)
-            newPop.append(p2)
-        else:
-            newOnes = crossover(p1, p2, n_cross)
-            newPop.extend(newOnes)
-
-    # mutate child
-    for i in range(len(newPop)):
-        nextOne = newPop[i]
-        doMutate = random.random()
-
-        # 50% chance of mutation
-
-        if doMutate <= 0.1:
-            nextOne.mutate()
-            newPop[i] = nextOne
-
-    return newPop
-
-def crossover(agent1, agent2, n_cross):
-    prob_cross = random.randint(0, 100)
-
-    if prob_cross <= 101:  # for future use
-
-        targets = random.sample(range(0, N_ATTRIBUTES), n_cross)
-
-        for i in targets:
-            attribute1 = agent1.get_e_attribute(i)
-            attribute2 = agent2.get_e_attribute(i)
-            agent1.set_e_attribute(i, attribute2)
-            agent2.set_e_attribute(i, attribute1)
-
-    return [agent1, agent2]
-
-
+main()
